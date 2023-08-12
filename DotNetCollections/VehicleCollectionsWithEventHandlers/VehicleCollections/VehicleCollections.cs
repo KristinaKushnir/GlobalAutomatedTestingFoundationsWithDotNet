fecedd6051a509da40bf1c4cf404ff9decd4a06d
kgkushnir <kristina.grintsevich@mail.ru>
2023-08-12 02:33:22 +0200

    Added the solution for task 'VehicleCollections' in section 'DotNetCollections'

---
+++ DotNetCollections/VehicleCollectionsWithEventHandlers/VehicleCollections/VehicleCollections.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Xml.Linq;
+
+namespace VehicleManagementApp
+{
+    public class VehicleCollection
+    {
+        static void Main(string[] args)
+        {
+            // Используем объекты из файла VehicleManagementApp.cs
+            Car car = new Car(new Engine("100hp", 1.8, "Gasoline", "ABC123"), new Chassis(4, "1234", 1000), new Transmission("Manual", 5, "Manufacturer"));
+            Truck truck = new Truck(new Engine("300hp", 5.0, "Diesel", "DEF456"), new Chassis(6, "5678", 5000), new Transmission("Automatic", 8, "Manufacturer"));
+            Bus bus = new Bus(new Engine("200hp", 2.5, "Gasoline", "GHI789"), new Chassis(4, "4321", 2000), new Transmission("Manual", 6, "Manufacturer"));
+            Scooter scooter = new Scooter(new Engine("50hp", 0.5, "Gasoline", "JKL012"), new Chassis(2, "9876", 100), new Transmission("Automatic", 1, "Manufacturer"));
+
+            // Создаем коллекцию транспортных средств
+            List<Car> vehicles = new List<Car>();
+
+            // Добавляем объекты в коллекцию
+            vehicles.Add(car);
+            vehicles.Add(truck);
+            vehicles.Add(bus);
+            vehicles.Add(scooter);
+
+            // Записываем информацию в XML файлы
+            WriteFullInfoToXml(vehicles);
+            WriteEngineInfoToXml(vehicles);
+            WriteGroupedInfoToXml(vehicles);
+        }
+
+        public static void WriteFullInfoToXml(List<Car> vehicles)
+        {
+            // Создаем XML документ для полной информации
+            XDocument fullInfoDocument = new XDocument(new XElement("Vehicles"));
+
+            // Фильтруем транспортные средства с объемом двигателя более 1.5 литров
+            var filteredVehicles = vehicles.Where(v => v.Engine.Volume > 1.5);
+
+            foreach (Car vehicle in filteredVehicles)
+            {
+                XElement vehicleElement = new XElement("Vehicle",
+                    new XElement("Type", vehicle.GetType().Name),
+                    new XElement("EnginePower", vehicle.Engine.Power),
+                    new XElement("EngineVolume", vehicle.Engine.Volume),
+                    new XElement("EngineType", vehicle.Engine.Type),
+                    new XElement("EngineSerialNumber", vehicle.Engine.SerialNumber),
+                    new XElement("ChassisWheels", vehicle.Chassis.NumberOfWheels),
+                    new XElement("ChassisNumber", vehicle.Chassis.Number),
+                    new XElement("ChassisLoadCapacity", vehicle.Chassis.LoadCapacity),
+                    new XElement("TransmissionType", vehicle.Transmission.Type),
+                    new XElement("TransmissionGears", vehicle.Transmission.NumberOfGears),
+                    new XElement("TransmissionManufacturer", vehicle.Transmission.Manufacturer));
+
+                fullInfoDocument.Root?.Add(vehicleElement);
+            }
+
+            // Сохраняем XML документ с полной информацией
+            fullInfoDocument.Save("full_info.xml");
+        }
+
+        public static void WriteEngineInfoToXml(List<Car> vehicles)
+        {
+            // Создаем XML документ для информации о двигателе
+            XDocument engineInfoDocument = new XDocument(new XElement("Vehicles"));
+
+            // Фильтруем автобусы и грузовики
+            var filteredVehicles = vehicles.Where(v => v is Bus || v is Truck);
+
+            foreach (Car vehicle in filteredVehicles)
+            {
+                XElement vehicleElement = new XElement("Vehicle",
+                    new XElement("Type", vehicle.GetType().Name),
+                    new XElement("EngineType", vehicle.Engine.Type),
+                    new XElement("EngineSerialNumber", vehicle.Engine.SerialNumber),
+                    new XElement("EnginePower", vehicle.Engine.Power));
+
+                engineInfoDocument.Root?.Add(vehicleElement);
+            }
+
+            // Сохраняем XML документ с информацией о двигателе
+            engineInfoDocument.Save("engine_info.xml");
+        }
+
+        public static void WriteGroupedInfoToXml(List<Car> vehicles)
+        {
+            // Создаем XML документ для группированной информации
+            XDocument groupedInfoDocument = new XDocument(new XElement("Vehicles"));
+
+            // Группируем транспортные средства по типу трансмиссии
+            var groupedVehicles = vehicles.GroupBy(v => v.Transmission.Type);
+
+            foreach (var group in groupedVehicles)
+            {
+                XElement transmissionElement = new XElement(group.Key);
+
+                foreach (Car vehicle in group)
+                {
+                    XElement vehicleElement = new XElement("Vehicle",
+                        new XElement("Type", vehicle.GetType().Name),
+                        new XElement("EnginePower", vehicle.Engine.Power),
+                        new XElement("EngineVolume", vehicle.Engine.Volume),
+                        new XElement("ChassisWheels", vehicle.Chassis.NumberOfWheels),
+                        new XElement("ChassisNumber", vehicle.Chassis.Number),
+                        new XElement("ChassisLoadCapacity", vehicle.Chassis.LoadCapacity),
+                        new XElement("TransmissionGears", vehicle.Transmission.NumberOfGears),
+                        new XElement("TransmissionManufacturer", vehicle.Transmission.Manufacturer));
+
+                    transmissionElement.Add(vehicleElement);
+                }
+
+                groupedInfoDocument.Root?.Add(transmissionElement);
+            }
+
+            // Сохраняем XML документ с группированной информацией
+            groupedInfoDocument.Save("grouped_info.xml");
+        }
+    }
+}
---
+++ DotNetCollections/VehicleCollectionsWithEventHandlers/VehicleManagementSystem/VehicleManagementApp.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace VehicleManagementApp
+{
+    public class Engine
+    {
+        public string Power { get; set; }
+        public double Volume { get; set; }
+        public string Type { get; set; }
+        public string SerialNumber { get; set; }
+
+        public Engine(string power, double volume, string type, string serialNumber)
+        {
+            Power = power;
+            Volume = volume;
+            Type = type;
+            SerialNumber = serialNumber;
+        }
+    }
+
+    public class Chassis
+    {
+        public int NumberOfWheels { get; set; }
+        public string Number { get; set; }
+        public double LoadCapacity { get; set; }
+
+        public Chassis(int numberOfWheels, string number, double loadCapacity)
+        {
+            NumberOfWheels = numberOfWheels;
+            Number = number;
+            LoadCapacity = loadCapacity;
+        }
+    }
+
+    public class Transmission
+    {
+        public string Type { get; set; }
+        public int NumberOfGears { get; set; }
+        public string Manufacturer { get; set; }
+
+        public Transmission(string type, int numberOfGears, string manufacturer)
+        {
+            Type = type;
+            NumberOfGears = numberOfGears;
+            Manufacturer = manufacturer;
+        }
+    }
+
+    public class Car
+    {
+        public Engine Engine { get; set; }
+        public Chassis Chassis { get; set; }
+        public Transmission Transmission { get; set; }
+
+        public Car(Engine engine, Chassis chassis, Transmission transmission)
+        {
+            Engine = engine;
+            Chassis = chassis;
+            Transmission = transmission;
+        }
+
+        public string GetCarInfo()
+        {
+            // Возвращает информацию о машине в виде строки
+            return $"Engine Power: {Engine.Power}, Volume: {Engine.Volume}, Type: {Engine.Type}, Serial Number: {Engine.SerialNumber}\n" +
+                   $"Chassis Wheels: {Chassis.NumberOfWheels}, Number: {Chassis.Number}, Load Capacity: {Chassis.LoadCapacity}\n" +
+                   $"Transmission Type: {Transmission.Type}, Gears: {Transmission.NumberOfGears}, Manufacturer: {Transmission.Manufacturer}";
+        }
+    }
+
+    public class Truck : Car
+    {
+        public Truck(Engine engine, Chassis chassis, Transmission transmission)
+            : base(engine, chassis, transmission)
+        {
+        }
+
+        public string GetTruckInfo()
+        {
+            // Возвращает информацию о грузовике, вызывая метод GetCarInfo() базового класса
+            return GetCarInfo();
+        }
+    }
+
+    public class Bus : Car
+    {
+        public Bus(Engine engine, Chassis chassis, Transmission transmission)
+            : base(engine, chassis, transmission)
+        {
+        }
+
+        public string GetBusInfo()
+        {
+            // Возвращает информацию о автобусе, вызывая метод GetCarInfo() базового класса
+            return GetCarInfo();
+        }
+    }
+
+    public class Scooter : Car
+    {
+        public Scooter(Engine engine, Chassis chassis, Transmission transmission)
+            : base(engine, chassis, transmission)
+        {
+        }
+
+        public string GetScooterInfo()
+        {
+            // Возвращает информацию о скутере, вызывая метод GetCarInfo() базового класса
+            return GetCarInfo();
+        }
+    }
+
+    class VehicleManagementSystem
+    {
+        static void Main(string[] args)
+        {
+            // Метод для парсинга аргументов и создания объектов для определенного типа транспортного средства
+            static void ParseVehicleArguments(string[] args, out Engine engine, out Chassis chassis, out Transmission transmission)
+            {
+                // Парсинг аргументов и создание объектов Engine, Chassis и Transmission
+                engine = new Engine(args[0], double.Parse(args[1]), args[2], args[3]);
+                chassis = new Chassis(int.Parse(args[4]), args[5], double.Parse(args[6]));
+                transmission = new Transmission(args[7], int.Parse(args[8]), args[9]);
+            }
+
+            Engine carEngine, truckEngine, busEngine, scooterEngine;
+            Chassis carChassis, truckChassis, busChassis, scooterChassis;
+            Transmission carTransmission, truckTransmission, busTransmission, scooterTransmission;
+
+            // Парсинг аргументов для автомобиля
+            ParseVehicleArguments(args[0..10], out carEngine, out carChassis, out carTransmission);
+
+            // Парсинг аргументов для грузовика
+            ParseVehicleArguments(args[10..20], out truckEngine, out truckChassis, out truckTransmission);
+
+            // Парсинг аргументов для автобуса
+            ParseVehicleArguments(args[20..30], out busEngine, out busChassis, out busTransmission);
+
+            // Парсинг аргументов для скутера
+            ParseVehicleArguments(args[30..40], out scooterEngine, out scooterChassis, out scooterTransmission);
+
+            // Создание объектов транспортных средств
+            Car car = new Car(carEngine, carChassis, carTransmission);
+            Truck truck = new Truck(truckEngine, truckChassis, truckTransmission);
+            Bus bus = new Bus(busEngine, busChassis, busTransmission);
+            Scooter scooter = new Scooter(scooterEngine, scooterChassis, scooterTransmission);
+
+            // Вывод информации о транспортных средствах
+            Console.WriteLine("Car specifications:");
+            Console.WriteLine(car.GetCarInfo());
+
+            Console.WriteLine("Truck specifications:");
+            Console.WriteLine(truck.GetTruckInfo());
+
+            Console.WriteLine("Bus specifications:");
+            Console.WriteLine(bus.GetBusInfo());
+            Console.WriteLine("Scooter specifications:");
+            Console.WriteLine(scooter.GetCarInfo());
+        }
+    }
+}
